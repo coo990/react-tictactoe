e049f4d527025898c9346b7f09613779ca5e2bfb
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-11-09 10:02:43 +0000

    refactor: cleaned up square functon to be a functional component

--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  // Now the state is stored in the Board component
   constructor(props) {
     super(props);
     this.state = {
